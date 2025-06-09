317090eb9677fe0b8b99e2ec91477b81a7c68ff9
chema <some@example.com>
2025-06-09 13:22:27 +0200

    Corregido problema con fichero swap de nano

--- controlador_dominio.sh
+++ controlador_dominio.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-echo "Creo el script"
+echo "Script de creacion de controlador de dominio Linux"
+
+# Primera parte: Creación del Creación
+
+mdadm --create --verbose /dev/md0 --level=5 --raid-devices=3 /dev/sdb /dev/sdc /dev/sdd
+# Otra forma:
+mdadm --create --verbose /dev/md0 --level=5 --raid-devices=3 /dev/sd{b..d}
